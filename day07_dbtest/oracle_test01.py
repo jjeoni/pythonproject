7e9ca16de9bb5ea47e92c3e9feea345e9c958ce6
jjeoni <wlgus9214@naver.com>
2022-05-20 17:28:47 +0900

    oracle 관련 소스 수정

--- day07_dbtest/oracle_test01.py
+++ day07_dbtest/oracle_test01.py
@@ -2,13 +2,11 @@ import cx_Oracle
 
 conn = cx_Oracle.connect('scott', 'tiger', '192.168.1.26:1521/xe')
 cursor = conn.cursor()
-cursor.execute('select * from emp')
+cursor.execute('select * from emp where deptno = 10')
 
 print(cursor)
 
 for item in cursor:
-    print(item[0:8])
+    print(item[1], item[5])
 
-# conn.close()
-
-# cursor.execute('select job from emp')
+conn.close()
