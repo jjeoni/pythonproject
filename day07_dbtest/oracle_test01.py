1e3ede57c1467b1786c9e353507a3dc4a22fa52c
jjeoni <wlgus9214@naver.com>
2022-05-23 14:51:41 +0900

    2022/05/23 오라클 라이브러리 설치 내용 추가

--- day07_dbtest/oracle_test01.py
+++ day07_dbtest/oracle_test01.py
@@ -1,10 +1,12 @@
+# pip install cx_Oracle
 import cx_Oracle
 
 conn = cx_Oracle.connect('scott', 'tiger', '192.168.1.26:1521/xe')
 cursor = conn.cursor()
 cursor.execute('select * from emp where deptno = 10')
 
-print(cursor)
+# print(cursor)
+
 
 for item in cursor:
     print(item[1], item[5])
