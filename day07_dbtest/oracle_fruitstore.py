28eb4e7a89619eae280401e774347afcc44e7651
jjeoni <wlgus9214@naver.com>
2022-05-24 14:20:54 +0900

    2022/05/24 pm2:20분 수정

--- day07_dbtest/oracle_fruitstore.py
+++ day07_dbtest/oracle_fruitstore.py
@@ -1,6 +1,6 @@
 import cx_Oracle
 
-conn = cx_Oracle.connect('scott', 'tiger', '192.168.1.26:1521/xe')
+conn = cx_Oracle.connect('scott', 'tiger', '192.168.1.4:1521/xe')
 cursor = conn.cursor()
 
 cursor.execute("drop table fruit_store")
@@ -13,52 +13,75 @@ while True:
     if choice == '1':
         sql = "insert into fruit_store values (:1, :2, :3)"
         cursor.execute('select * from fruit_store')
-
+        print('--------------------------------------------')
         for item in cursor:
-            print(item[0], item[1], item[2])
-        fruit_name = input('과일명 >>> ')
-        price = input('가격 >>> ')
-        count = input('수량 >>> ')
+            print(f'{item[0]} {item[1]}원 {item[2]}개')
+        print('--------------------------------------------')
+        fruit_name = input('추가할 과일명을 입력하세요 >>> ')
+        price = input('추가할 과일의 가격을 입력하세요 >>> ')
+        count = input('추가할 과일의 수량을 입력하세요 >>> ')
         data = (fruit_name, int(price), int(count))
         cursor.execute(sql, data)
+        print('--------------------------------------------')
+        cursor.execute('select * from fruit_store')
+        for item in cursor:
+            print(f'{item[0]} {item[1]}원 {item[2]}개')
+        print('--------------------------------------------')
         cursor.close
         conn.commit()       
 
 
     elif choice == '2':
         cursor.execute('select * from fruit_store')
+        print('--------------현재 재고현황---------------')
         for item in cursor:
-            print(item[0], item[1], item[2])
+            print(f'{item[0]} {item[1]}원 {item[2]}개')
+        print('--------------------------------------------')
 
-        fruits = input('수정할 과일 이름 >>> ')
-        modify_num = input('수정할 항목 (1. 가격 2. 개수) >>> ')
-        if modify_num == "1":
-            price2 = input('변경할 가격 >>> ')
+        fruits = input('수정할 과일 이름을 입력하세요 >>> ')
+        modify_num = input(''' 다음 중 수정할 항목을 선택하세요
+        price, count 중 입력 >>> ''')
+        if modify_num == "prcie":
+            price2 = input('변경할 가격을 입력하세요 >>> ')
             cursor.execute(f"update fruit_store set price = '{price2}' where fruit_name = '{fruits}'")
-        elif modify_num == "2":
-            count2 = input('변경할 수량 >>> ')
+        elif modify_num == "count":
+            count2 = input('변경할 수량을 입력하세요 >>> ')
             cursor.execute(f"update fruit_store set count = '{count2}' where fruit_name = '{fruits}'")
+        else :
+            print('다시 입력하세요')
+        print('--------------------------------------------')
+        cursor.execute('select * from fruit_store')
+        for item in cursor:
+            print(f'{item[0]} {item[1]}원 {item[2]}개')
+        print('--------------------------------------------')
         cursor.close
         conn.commit()
 
 
     elif choice == '3':
         cursor.execute('select * from fruit_store')
+        print('--------------현재 재고현황---------------')
         for item in cursor:
-            print(item[0], item[1], item[2])
-
-        delete_name = input('삭제할 과일 이름 >>> ')
+            print(f'{item[0]} {item[1]}원 {item[2]}개')
+        print('--------------------------------------------')
+        delete_name = input('삭제할 과일 이름을 입력하세요 >>> ')
         cursor.execute(f"delete from fruit_store where fruit_name = '{delete_name}'")
+        print(f'<<<<<<<<<{delete_name}이 삭제되었습니다.>>>>>>>>>')
+        cursor.execute('select * from fruit_store')
+        print('--------------삭제 후 재고현황---------------')
         for item in cursor:
-            print(item[0], item[1], item[2])
+            print(f'{item[0]} {item[1]}원 {item[2]}개')
+        print('--------------------------------------------')
         cursor.close
         conn.commit()
 
 
     elif choice == '4':
         cursor.execute("select * from fruit_store")
+        print('----------------재고 리스트-----------------')
         for item in cursor:
-            print(item[0], item[1], item[2])
+            print(f'{item[0]} {item[1]}원 {item[2]}개')
+        print('--------------------------------------------')
         cursor.close
         conn.commit()
 
